c7448e3f1167c0e7739a8023b7f1241355986c2c
Deep Patel <deep1907rp@gmail.com>
2024-12-26 14:02:35 +0530

    added vercel.json for reverse proxy

--- s3-reverse-proxy/index.js
+++ s3-reverse-proxy/index.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const httpProxy = require("http-proxy");
-
+const cors = require("cors");
 const app = express();
 const PORT = 8000;
 
-const BASE_PATH = process.env.BASE_PATH;
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
+
+const BASE_PATH =
+  "https://sitesync-output.s3.ap-south-1.amazonaws.com/__outputs";
 
 const proxy = httpProxy.createProxy();
 
 app.use((req, res) => {
   const hostname = req.hostname;
+
+  if (!hostname) {
+    return res.status(400).send("Invalid hostname");
+  }
+
   const subdomain = hostname.split(".")[0];
+
+  if (!subdomain) {
+    return res.status(400).send("Invalid subdomain");
+  }
 
   const resolvesTo = `${BASE_PATH}/${subdomain}`;
 
